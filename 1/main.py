3cb20bd5e82b4aabb50d56d7c8f8c27c4a045720
Niklavs-Veiksans <niklavs.veiksans@skolo.lv>
2024-01-23 17:51:44 +0200

    adsda

--- 1/main.py
+++ 1/main.py
@@ -1,26 +1,52 @@
 import PySimpleGUI as sg
 import datetime
 
-now = datetime.datetime.now()
-
+now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+paradit = []
 sg.theme('Hotdogstand')   
-
+fails = open("dati.txt", "a")
 layout = [  [sg.Text('RUN!')],
             [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
-
+            [sg.Button('OK'), sg.Button('Cancel')],
+            [sg.Column(paradit)] ]
 
 window = sg.Window('ParaDariPats', layout)
-
+paradumi = []
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout = 1000)
     if event == sg.WIN_CLOSED or event == 'Cancel': 
         break
-    print(values[0])
-    fails = open("dati.txt", "a")
-    piev = str(f'{now}: {values[0]}\n')
-    fails.write(piev)
-   
+
+    
+    elif event == 'OK':
+        print(values[0])
+        piev = str(f'{now}: {values[0]}\n')
+        fails.write(piev)
+
+
+        for val in values:
+            for par in paradumi:
+                if val == par:
+                    break
+                else:
+                     paradumi += piev
+                     paradit += [[sg.Button(par)]]
+                     window.refresh()
+                     break
+                   
+                    
+            if len(paradumi) == 0:
+                paradumi += piev
+                paradit += [[sg.Button(piev)]]
+                window.refresh()
+                
+
+
+        new_button = sg.Button('New Button')
+        # Update the layout by adding the new button to the existing layout
+
+        
+
 
 
 fails.close()
